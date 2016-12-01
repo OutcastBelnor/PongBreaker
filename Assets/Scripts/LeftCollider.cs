1c154a561b34ac6dadae3b613e957900ed0fea98
Jakub Janas <jakub.janas08@gmail.com>
2016-12-01 12:38:39 +0000

    Added collider script, boundaries of the scene, and the ball

---
+++ Assets/Scripts/LeftCollider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftCollider : MonoBehaviour
+{
+    public LevelManager levelManager;
+
+    public void OnTriggerEnter2D (Collider2D trigger)
+    {
+        levelManager.LoadLevel("Win");
+    }
+}
